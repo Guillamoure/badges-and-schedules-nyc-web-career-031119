94efa6001358566ab06a2d0722fe106e4527db57
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-24 20:36:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_bade_creator(attendees)
+def batch_badge_creator(attendees)
   badges = []
   attendees.each do |name|
     badges.push("Hello, my name is #{name}.")
   end
-  puts badges
   return badges
 end
