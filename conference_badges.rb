3cf5b5cbb48621db6267be95d90142abb558a8fc
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-24 20:39:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  room  = 1
   room_assignments = []
   attendees.each do |name|
-    room = 1
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{room}!")
     room += 1
   end
